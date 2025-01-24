b77db6a9d1d49a97267bdc0949abd42600a02103
Sam Richter <samrichter888@gmail.com>
2025-01-23 19:53:09 -0600

    word search done

--- begin.c
+++ begin.c
@@ -7,44 +7,48 @@
 
 #include "util.h"
 
-#define TABLE_MAX_DIM 20
-#define TABLE_BASE_LEN (TABLE_MAX_DIM * (TABLE_MAX_DIM * 2 + 1) + 1)
-#define TABLE_TEMP_LEN (TABLE_MAX_DIM * TABLE_MAX_DIM)
+#define CALC_MATRIX_BUFFERED_ROW_LEN(x) (x * 2)
+#define CALC_MATRIX_BUFFERED_SIZE(cols, rows) (cols * rows + 1)
 
-// #define DIR_DU 0
-// #define DIR_R 1
-// #define DIR_DD 2
-// #define DIR_D 3
+#define MATRIX_MAX_DIM          20
+#define MATRIX_STRING_ROW_LEN   CALC_MATRIX_BUFFERED_ROW_LEN(MATRIX_MAX_DIM)
+#define MATRIX_STRING_COL_LEN   (MATRIX_MAX_DIM)
+#define MATRIX_STRING_LEN       CALC_MATRIX_BUFFERED_SIZE(MATRIX_STRING_COL_LEN, MATRIX_STRING_ROW_LEN)
 
-const int32_t DIR_X_SCALAR[] = { 1, 1, 1, 0 };
-const int32_t DIR_Y_SCALAR[] = { -1, 0, 1, 1 };     // array y-axis goes down
+char string_matrix_1d[MATRIX_STRING_LEN];       // base buffer that can be directly printed
+char* string_matrix_2d[MATRIX_STRING_COL_LEN];  // index to each row
+uint32_t distance_field[MATRIX_MAX_DIM][MATRIX_MAX_DIM];
+
+MAKE_TYPED_MAX(uint32)
+MAKE_TYPED_MAX(int32)
+MAKE_TYPED_MIN(uint32)
+MAKE_TYPED_MIN(int32)
+
 
 typedef struct{ int32_t x, y; } Vec2i;
 typedef struct{ uint32_t x, y; } Vec2u;
 
-char table_base[TABLE_BASE_LEN];
-uint32_t availablity_status[TABLE_TEMP_LEN];
 
-uint32_t resolve_unbuffered_idx_2d(uint32_t x, uint32_t y, uint32_t dim)
+void initilize_2d_pointers(uint32_t row_len)
 {
-    return (y * (dim + 1) + x);
+    for(size_t i = 0; i < MATRIX_STRING_COL_LEN; i++)
+    {
+        string_matrix_2d[i] = string_matrix_1d + (i * row_len * sizeof(char));
+    }
 }
-uint32_t resolve_buffered_idx_2d(uint32_t x, uint32_t y, uint32_t dim)
+
+uint32_t buffered_spaced_row_len(uint32_t dim)
 {
-    return (y * (dim + 2) + x);
+    return CALC_MATRIX_BUFFERED_ROW_LEN(dim);
 }
-uint32_t resolve_buffered_idx_1d(uint32_t x, uint32_t dim)
+uint32_t buffered_string_matrix_len(uint32_t dim)
 {
-    return x + (x / dim);
+    return CALC_MATRIX_BUFFERED_SIZE(dim, buffered_spaced_row_len(dim));
 }
 
 uint32_t rand_direction()
 {
     return (uint32_t)rand() % 4;
-}
-uint32_t next_direction(uint32_t dir)
-{
-    return (dir + 1) % 4;
 }
 
 Vec2u rand_position(uint32_t dim)
@@ -55,114 +59,33 @@ Vec2u rand_position(uint32_t dim)
     return v;
 }
 
-MAKE_TYPED_MAX(uint32)
-MAKE_TYPED_MAX(int32)
-MAKE_TYPED_MIN(uint32)
-MAKE_TYPED_MIN(int32)
-
-
-
-Vec2i offset_position_2d(
-    uint32_t x,
-    uint32_t y,
-    uint32_t n,
-    uint32_t dir )
-{
-    Vec2i v;
-    v.x = (int32_t)x + (int32_t)n * DIR_X_SCALAR[dir];
-    v.y = (int32_t)y + (int32_t)n * DIR_Y_SCALAR[dir];
-    return v;
-}
-
-bool resolve_does_fit(
-    uint32_t x,
-    uint32_t y,
-    uint32_t n,
-    uint32_t dir,
-    uint32_t dim )
-{
-    const Vec2i v = offset_position_2d(x, y, n, dir);
-    return (v.x >= 0 && v.x <= dim && v.y >= 0 && v.y <= dim);
-}
-
-uint32_t get_buffered_nth_letter_idx(
-    uint32_t x,
-    uint32_t y,
-    uint32_t n,
-    uint32_t dir,
-    uint32_t dim )
-{
-    const Vec2i v = offset_position_2d(x, y, n, dir);
-    return resolve_buffered_idx_2d(v.x, v.y, dim);
-}
-
-uint32_t get_unbuffered_nth_letter_idx(
-    uint32_t x,
-    uint32_t y,
-    uint32_t n,
-    uint32_t dir,
-    uint32_t dim )
-{
-    const Vec2i v = offset_position_2d(x, y, n, dir);
-    return resolve_unbuffered_idx_2d(v.x, v.y, dim);
-}
-
-uint32_t get_max_length(uint32_t x, uint32_t y, uint32_t dir, uint32_t dim)
-{
-    const int32_t x_scalar = DIR_X_SCALAR[dir];
-    const int32_t y_scalar = DIR_Y_SCALAR[dir];
-
-    if(x >= dir || y >= dir || x < 0 || y < 0) return 0;
-
-    switch(dir % 4)
-    {
-        case 0: return uint32_min(dim - x, y + 1);
-        case 1: return dim - x;
-        case 2: return uint32_min(dim - x, dim - y);
-        case 3: return dim - y;
-        default: return 0;
-    }
-}
-
-uint8_t get_avail_len(uint32_t avail, uint32_t dir)
-{
-    return avail & (0xFF << (dir & 0x4) * 8);
-}
-void set_avail_len(uint32_t* avail, uint32_t dir, uint8_t len)
-{
-    const uint32_t shift = (dir & 0x4) * 8;
-    *avail = (*avail & !(0xFF << shift)) | ((uint32_t)(len & 0xFF) << shift);
-}
-
-
-
 
 
 void initialize(uint32_t dim)
 {
     srand(time(NULL));
+    const uint32_t
+        matx_size = buffered_string_matrix_len(dim),
+        matx_row_size = buffered_spaced_row_len(dim);
 
-    // for(int i = 0; i < TABLE_BASE_LEN; i++)
-    // {
-    //     table_base[i] = 'a' + (char)(rand() % 26);
-    // }
-    memset(table_base, ' ', TABLE_BASE_LEN);
+    memset(string_matrix_1d, ' ', matx_size);
+    string_matrix_1d[matx_size - 1] = '\0';
+    initilize_2d_pointers(matx_row_size);
 
-    table_base[dim * (dim * 2 + 1)] = '\0';
-    for(int i = 0; i < dim; i++)
+    for(uint32_t i = 0; i < dim; i++)
     {
-        table_base[resolve_unbuffered_idx_2d(i, dim * 2, dim * 2)] = '\n';
+        string_matrix_2d[i][matx_row_size - 1] = '\n';
     }
 
-    for(uint32_t x = 0; x < dim; x++)
+    for(uint32_t y = 0; y < dim; y++)
     {
-        for(uint32_t y = 0; y < dim; y++)
+        for(uint32_t x = 0; x < dim; x++)
         {
-            uint32_t* avail = availablity_status + (uint32_t)resolve_unbuffered_idx_2d(x, y, dim);
-            for(uint32_t d = 0; d < 4; d++)
-            {
-                set_avail_len(avail, d, (uint8_t)get_max_length(x, y, d, dim));
-            }
+            distance_field[y][x] =
+                ((uint32_min(dim - x, y + 1)    & 0xFF) <<  0) |
+                (((dim - x)                     & 0xFF) <<  8) |
+                ((uint32_min(dim - x, dim - y)  & 0xFF) << 16) |
+                (((dim - y)                     & 0xFF) << 24);
         }
     }
 }
@@ -171,37 +94,48 @@ bool try_insert_string(const char* str, uint32_t dim)
 {
     const size_t len = strlen(str);
     // printf("trying to insert: %s -- len: %lu\n", str, len);
-    if(len > dim || len == 0) return false;
-
-    Vec2u begin_loc = rand_position(dim);
-    uint32_t begin_dir = rand_direction();
-    // printf("rand pos: (%u, %u), rand dir: %u\n", begin_loc.x, begin_loc.y, begin_dir);
-    for(uint32_t _x = 0; _x < dim; _x++)
+    if(len > dim || len == 0)
     {
-        const uint32_t x = (_x + begin_loc.x) % dim;
-        for(uint32_t _y = 0; _y < dim; _y++)
+        printf("Failed to insert string: \"%s\" (invalid length)\n", str);
+        return false;
+    }
+
+    const Vec2u begin_loc = rand_position(dim);
+    const uint32_t begin_dir = rand_direction();
+    // printf("rand pos: (%u, %u), rand dir: %u\n", begin_loc.x, begin_loc.y, begin_dir);
+    for(uint32_t _y = 0; _y < dim; _y++)
+    {
+        const uint32_t y = (_y + begin_loc.y) % dim;
+        for(uint32_t _x = 0; _x < dim; _x++)
         {
-            const uint32_t y = (_y + begin_loc.y) % dim;
+            const uint32_t x = (_x + begin_loc.x) % dim;
             for(uint32_t _d = 0; _d < 4; _d++)
             {
                 const uint32_t d = (_d + begin_dir) % 4;
-                const uint32_t* avail = availablity_status + resolve_unbuffered_idx_2d(x, y, dim);
-                const uint32_t avail_len = get_avail_len(*avail, d);
+                const uint32_t avail_len = (distance_field[y][x] >> (d * 8)) & 0xFF;
                 // printf("trying to insert at position: (%u, %u) -- dir: %u -- avail: %u\n", x, y, d, avail_len);
                 if(len <= avail_len)  // if can fit at this pose
                 {
+                    static const int32_t DIR_X_SCALAR[] = { 1, 1, 1, 0 };
+                    static const int32_t DIR_Y_SCALAR[] = { -1, 0, 1, 1 };     // array y-axis goes down
+
                     uint32_t n;
                     for(n = 0; n < len; n++)   // check all characters to find collisions
                     {
-                        const uint32_t idx = get_buffered_nth_letter_idx(x * 2, y, n, d, dim * 2);
-                        if(table_base[idx] != ' ' && table_base[idx] != str[n]) break;
+                        const int32_t vy = (int32_t)y + (int32_t)n * DIR_Y_SCALAR[d];
+                        const int32_t vx = (int32_t)x + (int32_t)n * DIR_X_SCALAR[d];
+
+                        const char v = string_matrix_2d[vy][vx * 2];
+                        if(v != ' ' && v != str[n]) break;
                     }
                     if(n == len)
                     {
                         for(n = 0; n < len; n++)
                         {
-                            const uint32_t idx = get_buffered_nth_letter_idx(x * 2, y, n, d, dim * 2);
-                            table_base[idx] = str[n];
+                            const int32_t vy = (int32_t)y + (int32_t)n * DIR_Y_SCALAR[d];
+                            const int32_t vx = (int32_t)x + (int32_t)n * DIR_X_SCALAR[d];
+
+                            string_matrix_2d[vy][vx * 2] = str[n];
                         }
                         return true;
                     }
@@ -210,21 +144,36 @@ bool try_insert_string(const char* str, uint32_t dim)
         }
     }
 
+    printf("Failed to insert string: \"%s\" (could not find compatible position)\n", str);
     return false;
 }
 
 void fill_random(uint32_t dim)
 {
-    for(uint32_t i = 0; i < (dim * dim); i++)
+    for(uint32_t y = 0; y < dim; y++)
     {
-        const uint32_t idx = resolve_buffered_idx_1d(i * 2, dim * 2);
-        if(table_base[idx] == ' ') table_base[idx] = 'a' + (char)(rand() % 26);
+        for(uint32_t x = 0; x < dim; x++)
+        {
+            char* v = string_matrix_2d[y] + x * 2;
+            if(*v == ' ') *v = 'a' + (char)(rand() % 26);
+        }
     }
 }
 
-void print_table()
+void print_word_search(uint32_t n, char** words, uint32_t dim)
 {
-    printf("%s", table_base);
+    const uint32_t row_chars = dim * 2 - 1;
+    printf("+%.*s+\n", row_chars + 2, "-----------------------------------------");
+    for(uint32_t y = 0; y < dim; y++)
+    {
+        printf("| %.*s |\n", row_chars, string_matrix_2d[y]);
+    }
+    printf("+%.*s+\n", row_chars + 2, "-----------------------------------------");
+    printf("Words to find:\n");
+    for(uint32_t s = 0; s < n; s++)
+    {
+        printf("%d. %s\n", s + 1, words[s]);
+    }
 }
 
 
@@ -233,20 +182,20 @@ int main(int argc, char** argv)
     if(argc < 2) return -1;
 
     int32_t table_dim = atoi(argv[1]);
-    if(table_dim > TABLE_MAX_DIM) table_dim = 20;
-    if(table_dim < 0) table_dim = 5;
+    if(table_dim > MATRIX_MAX_DIM) table_dim = 20;
+    if(table_dim < 0) table_dim = 10;
 
     initialize(table_dim);
     for(int s = 2; s < argc; s++)
     {
         if(!try_insert_string(argv[s], table_dim))
         {
-            printf("Failed to insert string: %s\n", argv[s]);
             return -2;
         }
     }
     fill_random(table_dim);
-    print_table();
+
+    print_word_search(argc - 2, argv + 2, table_dim);
 
     return 0;
 }
